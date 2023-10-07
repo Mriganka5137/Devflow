0894322c0a047b2e9972a4321032a1dee652b5e4
Mriganka5137 <gogoimriganka09@gmail.com>
2023-10-07 09:36:36 +0530

    updates

--- components/shared/sidebar/LeftSidebar.tsx
+++ components/shared/sidebar/LeftSidebar.tsx
@@ -6,38 +6,7 @@ import React from "react";
 
 const LeftSidebar = () => {
   return (
-    <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
-      <section className="flex h-full flex-col gap-6 pt-16">
-        {sidebarLinks.map((item) => {
-          const isActive =
-            (pathname.includes(item.route) && item.route.length > 1) ||
-            pathname === item.route;
-          return (
-            <SheetClose asChild key={item.route}>
-              <Link
-                href={item.route}
-                className={`${
-                  isActive
-                    ? "primary-gradient rounded-lg text-light-900"
-                    : "text-dark300_light900 "
-                } flex items-center justify-start gap-4 bg-transparent p-4 `}
-              >
-                <Image
-                  src={item.imgURL}
-                  alt={item.label}
-                  width={20}
-                  height={20}
-                  className={`${isActive ? "" : "invert-colors"}`}
-                />
-                <p className={`${isActive ? "base-bold" : "base-medium"}`}>
-                  {item.label}
-                </p>
-              </Link>
-            </SheetClose>
-          );
-        })}
-      </section>
-    </section>
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]"></section>
   );
 };
 
--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -12,22 +12,18 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
   const handleThemeChange = () => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
+    if (mode === "dark") {
+      setMode("light");
+      document.documentElement.classList.add("light");
+    } else {
       setMode("dark");
       document.documentElement.classList.add("dark");
-    } else {
-      setMode("light");
-      document.documentElement.classList.remove("dark");
     }
   };
 
   useEffect(() => {
     handleThemeChange();
-  }, [mode]);
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
@@ -41,5 +37,4 @@ export function useTheme() {
   if (context === undefined) {
     throw new Error("useTheme must be used within a ThemeProvider");
   }
-  return context;
 }
