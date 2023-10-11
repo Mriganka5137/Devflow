68a72b29592310c815e16949a5b03e16e5a6a8ba
Mriganka5137 <gogoimriganka09@gmail.com>
2023-10-11 12:51:05 +0530

    Ask Question form complete

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Question from "@/components/forms/Question";
+import React from "react";
 
-const page = () => {
+const Page = () => {
   return (
-    <div>Ask Question</div>
-  )
-}
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div>
+        <Question />
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default Page;
---
+++ lib/actions/question.action.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-useless-catch */
+"use server";
+
+export async function createQuestions(params) {
+  try {
+    // connect to DB
+  } catch (err) {
+    throw err;
+  }
+}
