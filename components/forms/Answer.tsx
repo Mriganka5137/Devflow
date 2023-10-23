014e5ac188324f82a00e4e6ea54941571fb8f4b2
Mriganka5137 <gogoimriganka09@gmail.com>
2023-10-23 12:59:13 +0530

    Add Answer Form

---
+++ components/forms/Answer.tsx
@@ -0,0 +1,132 @@
+"use client";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "../ui/form";
+import { AnswerSchema } from "@/lib/validations";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Editor } from "@tinymce/tinymce-react";
+import { useRef, useState } from "react";
+import { useTheme } from "@/context/ThemeProvider";
+import { Button } from "../ui/button";
+import Image from "next/image";
+
+const Answer = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { mode } = useTheme();
+  const editorRef = useRef(null);
+  const form = useForm<z.infer<typeof AnswerSchema>>({
+    resolver: zodResolver(AnswerSchema),
+    defaultValues: {
+      answer: "",
+    },
+  });
+
+  const handleCreateAnswer = (data) => {};
+
+  return (
+    <div>
+      <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+        <h4 className="paragraph-semibold text-dark400_light800">
+          Write your answers here
+        </h4>
+        <Button
+          className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
+          onClick={() => {}}
+        >
+          <Image
+            src="/assets/icons/stars.svg"
+            width={12}
+            height={12}
+            alt="star"
+            className="object-contain"
+          />
+          Generate an AI answer
+        </Button>
+      </div>
+      <Form {...form}>
+        <form
+          className="mt-6 flex w-full flex-col gap-10"
+          onSubmit={form.handleSubmit(handleCreateAnswer)}
+        >
+          <FormField
+            control={form.control}
+            name="answer"
+            render={({ field }) => (
+              <FormItem className=" flex w-full flex-col">
+                <FormControl className="mt-3.5">
+                  <Editor
+                    apiKey={process.env.NEXT_PUBLIC_TINY_MCE_API_KEY}
+                    onInit={(evt, editor) => {
+                      // @ts-ignore
+                      editorRef.current = editor;
+                    }}
+                    onBlur={field.onBlur}
+                    onEditorChange={(content) => {
+                      field.onChange(content);
+                    }}
+                    initialValue=""
+                    init={{
+                      height: 350,
+                      menubar: false,
+                      plugins: [
+                        "advlist",
+                        "autolink",
+                        "lists",
+                        "link",
+                        "image",
+                        "charmap",
+                        "print",
+                        "preview",
+                        "anchor",
+                        "searchreplace",
+                        "visualblocks",
+                        "codesample",
+                        "code",
+                        "fullscreen",
+                        "insertdatetime",
+                        "media",
+                        "table",
+                        "paste",
+                        "code",
+                        "help",
+                        "wordcount",
+                      ],
+                      toolbar:
+                        "undo redo | " +
+                        "codesample | bold italic backcolor | alignleft aligncenter " +
+                        "alignright alignjustify | bullist numlist | " +
+                        "removeformat | help",
+                      content_style:
+                        "body { font-family:Inter; font-size:16px }",
+                      skin: mode === "dark" ? "oxide-dark" : "oxide",
+                      content_css: mode === "dark" ? "dark" : "light",
+                    }}
+                  />
+                </FormControl>
+                <FormMessage className="text-red-500" />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-end">
+            <Button
+              type="button"
+              className="primary-gradient w-fit text-white"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Submitting" : "Submit"}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default Answer;
