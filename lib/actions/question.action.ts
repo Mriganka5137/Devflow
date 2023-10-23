fae311c3e20d99dae76f89f719b1f0dca6993ceb
Mriganka5137 <gogoimriganka09@gmail.com>
2023-10-23 11:33:03 +0530

    Question Detail Page Setup

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -3,7 +3,11 @@
 import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
 import Tag from "@/database/tag.model";
-import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
+import {
+  CreateQuestionParams,
+  GetQuestionByIdParams,
+  GetQuestionsParams,
+} from "./shared.types";
 import User from "@/database/user.model";
 import { revalidatePath } from "next/cache";
 
@@ -65,6 +69,32 @@ export async function createQuestions(params: CreateQuestionParams) {
   } catch (err) {
     console.log(err);
     throw err;
+  }
+}
+
+export async function getQuestionByID(params: GetQuestionByIdParams) {
+  try {
+    connectToDatabase();
+
+    const { questionId } = params;
+
+    // Get the question data
+    const question = await Question.findById(questionId)
+      .populate({
+        path: "tags",
+        model: Tag,
+        select: "_id name",
+      })
+      .populate({
+        path: "author",
+        model: "User",
+        select: "_id clerkId name picture",
+      });
+
+    return question;
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
 }
 
