2a311d8fde7fa264880a242c1d68e07c843dd116
Mriganka5137 <gogoimriganka09@gmail.com>
2023-10-26 12:40:07 +0530

    Add Tag Details Page

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,63 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import NoResults from "@/components/shared/NoResults";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { IQuestion } from "@/database/question.model";
+import { getQuestionsByTagId } from "@/lib/actions/tag.actions";
+import { URLProps } from "@/types";
+import React from "react";
+
+const Page = async ({ params, searchParams }: URLProps) => {
+  const result = await getQuestionsByTagId({
+    tagId: params.id,
+    page: 1,
+    searchQuery: searchParams.q,
+  });
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
+
+      {/* SEARCHBARS */}
+      <div className="mt-11 w-full">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search questions"
+          otherClasses="flex-1"
+        />
+      </div>
+
+      {/* QUESTION CARD */}
+      <div className="mt-10 flex w-full flex-col gap-6 ">
+        {result && result.questions && result.questions.length > 0 ? (
+          result.questions.map((question: IQuestion) => {
+            return (
+              <QuestionCard
+                _id={question._id}
+                key={question._id}
+                title={question.title}
+                tags={question.tags}
+                author={question.author}
+                upvotes={question.upvotes}
+                answers={question.answers}
+                views={question.views}
+                createdAt={question.createdAt}
+              />
+            );
+          })
+        ) : (
+          <NoResults
+            title=" There’s no saved questions to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+      discussion. our query could be the next big thing others learn from. Get
+      involved! 💡"
+            link="/ask-question"
+            linkText="Ask a Question"
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Page;
