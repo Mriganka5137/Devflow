c583b66a6a4d80542c63c636360c503df96a1501
Mriganka5137 <gogoimriganka09@gmail.com>
2023-10-26 09:39:01 +0530

    Complete Collections Page

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -1,38 +1,27 @@
-import HomeFilters from "@/components/home/HomeFilters";
 import QuestionCard from "@/components/cards/QuestionCard";
 import Filter from "@/components/shared/Filter";
 import NoResults from "@/components/shared/NoResults";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
-import { Button } from "@/components/ui/button";
-import { HomePageFilters } from "@/constants/filters";
-import Link from "next/link";
-import { getQuestions } from "@/lib/actions/question.action";
+import { QuestionFilters } from "@/constants/filters";
+import { getSavedQuestions } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
+export default async function Collection() {
   // Get the Questions from DB
-  let result;
+  const { userId } = auth();
+  // const userId = "12345";
+  if (!userId) redirect("/sign-in");
 
-  try {
-    // Get the Questions from DB
-    result = await getQuestions({});
-  } catch (error) {
-    // Handle any errors that occur during the data fetching
-    console.error("Error fetching questions:", error);
-  }
+  // Get the Questions from DB
+  const result = await getSavedQuestions({ clerkId: userId });
 
   return (
     <>
-      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center ">
-        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
-        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
-          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
-            Ask a Question
-          </Button>
-        </Link>
-      </div>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
 
       {/* SEARCHBARS */}
-      <div className="mt-11 flex h-40 flex-col justify-between gap-2  max-md:flex-row max-sm:flex-col max-sm:justify-evenly sm:items-start">
+      <div className="mt-11 flex justify-between gap-2  max-md:flex-row max-sm:flex-col max-sm:justify-evenly sm:items-start">
         <LocalSearchbar
           route="/"
           iconPosition="left"
@@ -41,17 +30,15 @@ export default async function Home() {
           otherClasses="flex-1"
         />
         <Filter
-          filters={HomePageFilters}
+          filters={QuestionFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
-          containerClasses="hidden max-md:flex"
         />
-        <HomeFilters />
       </div>
 
       {/* QUESTION CARD */}
       <div className="mt-10 flex w-full flex-col gap-6 ">
         {result && result.questions && result.questions.length > 0 ? (
-          result.questions.map((question) => {
+          result.questions.map((question: any) => {
             return (
               <QuestionCard
                 _id={question._id}
@@ -68,7 +55,7 @@ export default async function Home() {
           })
         ) : (
           <NoResults
-            title=" There’s no question to show"
+            title=" There’s no saved questions to show"
             description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
           discussion. our query could be the next big thing others learn from. Get
           involved! 💡"
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -6,11 +6,14 @@ import {
   CreateUserParams,
   DeleteUserParams,
   GetAllUsersParams,
+  GetSavedQuestionsParams,
   ToggleSaveQuestionParams,
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
+import Tag from "@/database/tag.model";
+import { FilterQuery } from "mongoose";
 
 // Get User By Id Server Action
 export async function getUserById(params: any) {
@@ -134,6 +137,50 @@ export async function toggleSaveQuestion(params: ToggleSaveQuestionParams) {
     }
 
     revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getSavedQuestions(params: GetSavedQuestionsParams) {
+  /*
+    clerkId: string;
+  page?: number;
+  pageSize?: number;
+  filter?: string;
+  searchQuery?: string;
+  
+  */
+
+  try {
+    connectToDatabase();
+    const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+
+    const query: FilterQuery<typeof Question> = searchQuery
+      ? {
+          title: { $regex: new RegExp(searchQuery, "i") },
+        }
+      : {};
+
+    const user = await User.findOne({ clerkId }).populate({
+      path: "saved",
+      match: query,
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name picture" },
+      ],
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const savedQuestions = user.saved;
+
+    return { questions: savedQuestions };
   } catch (error) {
     console.log(error);
   }
